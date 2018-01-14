994f2e89c37b6d9ea5bc89a68a4102b582d679b5
Diego Gonzalez <Diego Gonzalez@DESKTOP-J9FP4RK>
2018-01-14 11:17:03 +0100

    Frankfurter sundays change

--- newspapers/src/downloader/FrankfurterDownloader.java
+++ newspapers/src/downloader/FrankfurterDownloader.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 
@@ -23,8 +25,8 @@ import com.machinepublishers.jbrowserdriver.Settings;
 
 public class FrankfurterDownloader {
 
-	private static final String geckoPath = "/usr/bin/geckodriver";
-	//private static final String geckoPath = "C:\\Users\\Diego Gonzalez\\git\\newspapers\\newspapers\\lib\\browserDrivers\\geckodriver.exe";
+	//private static final String geckoPath = "/usr/bin/geckodriver";
+	private static final String geckoPath = "C:\\Users\\Diego Gonzalez\\git\\newspapers\\newspapers\\lib\\browserDrivers\\geckodriver.exe";
 	private static final String FrankfurterUrl = "https://epaper.faz.net/";
 
 	private static String downloadPath = null;
@@ -33,6 +35,9 @@ public class FrankfurterDownloader {
 	private static DateFormat dateFormat1 = new SimpleDateFormat("dd.MM.yyyy");
 
 	private static String currentDate1;
+
+	private static String urlTypeOfPaper;
+	private static String xpathTypeOfPaper;
 
 	private static ArrayList<String> urls = null;
 
@@ -55,9 +60,24 @@ public class FrankfurterDownloader {
 
 			urls = new ArrayList<String>();
 
+			// PAPER NAME VARS
+			urlTypeOfPaper = "FAZ";
+			xpathTypeOfPaper = "F.A.Z.";
+
 			// Current Date
 			Date date = new Date();
 			currentDate1 = dateFormat1.format(date);
+
+			GregorianCalendar cal = new GregorianCalendar();
+			cal.setTime(date);
+			int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+
+			// DIFF PAPER ON SUNDAYS
+			if (dayOfWeek == Calendar.SUNDAY) {
+				urlTypeOfPaper = "FAS";
+				xpathTypeOfPaper = "F.A.S.";
+			}
+
 			System.out.println("Current Date: " + currentDate1);
 
 		} else {
@@ -74,14 +94,14 @@ public class FrankfurterDownloader {
 		System.out.println("1. Start Login");
 
 		// 2. Go to Frankfurter page
-		driver.get(FrankfurterUrl + "FAZ/" + currentDate1);
+		driver.get(FrankfurterUrl + urlTypeOfPaper + "/" + currentDate1);
 
 		WebDriverWait wait = new WebDriverWait(driver, 30);
 
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//img[@alt='F.A.Z.']")));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//img[@alt='" + xpathTypeOfPaper + "']")));
 
 		// 3. Login
-		driver.findElement(By.xpath("//img[@alt='F.A.Z.']")).click();
+		driver.findElement(By.xpath("//img[@alt='" + xpathTypeOfPaper + "']")).click();
 
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("loginName")));
 
@@ -95,12 +115,13 @@ public class FrankfurterDownloader {
 
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("newspapers-row")));
 
-		// 4. Go to paper
-		driver.get(FrankfurterUrl + "FAZ/" + currentDate1);
+		// 4. Go to paper (depend on the day)
+		driver.get(FrankfurterUrl + urlTypeOfPaper + "/" + currentDate1);
 
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//img[contains(@alt,\"F.A.Z\")]")));
+		wait.until(ExpectedConditions
+				.visibilityOfElementLocated(By.xpath("//img[contains(@alt,\"" + xpathTypeOfPaper + "\")]")));
 
-		WebElement imgLink = driver.findElement(By.xpath("//img[contains(@alt,\"F.A.Z\")]"));
+		WebElement imgLink = driver.findElement(By.xpath("//img[contains(@alt,\"" + xpathTypeOfPaper + "\")]"));
 
 		// 5. Format final url
 		String rawLink = imgLink.getAttribute("src");
@@ -111,7 +132,7 @@ public class FrankfurterDownloader {
 
 		System.out.println(rawLink);
 
-		urls.add("FAZ|" + rawLink);
+		urls.add(urlTypeOfPaper + "|" + rawLink);
 
 		// Write urls to file
 		BufferedWriter outputWriter;
